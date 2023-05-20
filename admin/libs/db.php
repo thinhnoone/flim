ab13d9b8c1f778ed35bf759e97d1f1636367fec4
thinhnoone <132722457+thinhnoone@users.noreply.github.com>
2023-05-20 07:52:17 +0700

    les

--- admin/libs/db.php
+++ admin/libs/db.php
@@ -1,8 +1,8 @@
 <?php
-	$host='localhost';
-	$user='root';
-	$pass='vertrigo';
-	$db='film';
+	$host="localhost";
+	$user="root";
+	$pass="vertrigo";
+	$db="film";
 	//error_reporting(0);//Chan thong bao loi
 
 	$link=mysqli_connect($host,$user,$pass,$db) or die('Lỗi kết nối');
